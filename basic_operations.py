c4335bd9de4bbae4cbf939a98d0cec1c6b5f0e0e
IgnacioGMG <igmg@igmg.com.ar>
2021-06-30 22:52:20 -0300

    first basic operations version

---
+++ basic_operations.py
@@ -0,0 +1,22 @@
+# The Complete Python Course For Beginners - @credits Tech With Tim
+# Beginner Python Programming
+#
+# Basic Operators & Input
+
+# Input and printing
+nombre = input("Cómo te llamas? ")
+age = input("Cuál es tu edad? ")
+print("Mi edad es", age, "y me llamo", nombre)  # Una forma de hacerlo
+print(f"Mi edad es {age} y me llamo {nombre}")  # Otra forma de hacerlo (admite formteo de variables)
+
+# Operators
+
+num1 = 2
+num2 = 3.4
+num3 = -5
+num4 = 1
+
+print(num1 + num2)
+print(num3 ** 2 // 2)  # Potencia y división entera
+print(num3 ** 2 % 2)  # Resto
+print(num3 % 2)  # Resto
