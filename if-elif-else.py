2f58192d611fc830b9b67dfe75abcc21e54e93c5
IgnacioGMG <igmg@igmg.com.ar>
2021-07-02 20:24:31 -0300

    agregado if-elif-else

---
+++ if-elif-else.py
@@ -0,0 +1,13 @@
+# The Complete Python Course For Beginners - @credits Tech With Tim
+# Beginner Python Programming
+#
+# If-Elif-Else
+
+age = input ( 'Ingresa tu edad: ')
+
+if int(age) == 16:
+    print(f'Tienes 16 años')
+elif int(age) > 16:
+    print(f'Tienes más de 16 años')
+else:
+    print(f'Tienes menos de 16 años')
