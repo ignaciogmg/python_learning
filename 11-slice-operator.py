a9d14fbf623d608c75940fb26cfbcb94d52a8d18
IgnacioGMG <igmg@igmg.com.ar>
2021-07-02 22:55:08 -0300

    file inserted

---
+++ 11-slice-operator.py
@@ -0,0 +1,12 @@
+# The Complete Python Course For Beginners - @credits Tech With Tim
+# Beginner Python Programming
+#
+# Slice operator
+
+fruits = ['banana', 'pears', 'strawberry']
+text = 'Hello to all the human beings'
+
+print(text[3: 10: 2])  # [start:stop:step]
+
+fruits[1:1] = ["pomelo"]  # Si no pongo los corchetes va a ingresar cada letra de la palabra como un ítem.
+print(fruits)
