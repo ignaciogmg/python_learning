ee41b25782c4a6697a2163d88721ab198580d332
IgnacioGMG <igmg@igmg.com.ar>
2021-07-27 19:46:35 -0300

    added file

--- tandemBicycle.py
+++ tandemBicycle.py
@@ -1,20 +1,9 @@
-import turtle
-from math import log, sqrt
+def saludar():
+    """
+Esta función es sólo un ejemplo,
+para ver como funciona la documentación
+incluida como docstring.
+    """
+    print("Hola Ñaño")
 
-window = turtle.Screen()
-window.bgcolor("black")
-
-anita = turtle.Turtle()
-anita.speed(0)
-anita.color("yellow")
-anita.pensize(5)
-
-anita.penup()
-anita.goto(-100, -200)
-anita.pendown()
-
-for n in range(1, 7400, 5):
-    anita.left(sqrt(n / 2 * (n + 60)))
-    anita.forward(log(n + n / 2))
-
-window.exitonclick()
+print(saludar.__doc__)
